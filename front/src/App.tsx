3c84d5914c7e6add3d7d82904eb015119104aaad
sphinx <okhaled32111@gmail.com>
2025-12-06 11:26:08 +0200

    final front

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  const [count, setCount] = React.useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(count + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [count]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        </p> */}
+        <p>{count}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reac
         </a>
       </header>
     </div>
